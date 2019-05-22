1cfed3917210e5f21c571afb252b2e5c9b0f0c91
Kane Koo <kanekoo@gmail.com>
2019-05-22 00:48:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram 
+  
+  attr_accessor word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    matches=[]
+    words.each do |testword|
+      wordarr=testword.split("").sort 
+      word_org_split=@word.split("").sort
+      if wordarr==word_org_split
+        matches << testword
+      end
+    end
+  end
+      
+      
+end
